3cd92e059374f7b7ee9c51e69a9f2f6d70bc7605
tutkuince <tutku.ince@outlook.com>
2019-12-02 22:06:15 +0300

    passing more properties

--- src/index.js
+++ src/index.js
@@ -1,14 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import faker from 'faker';
+
 import CommentDetail from "./CommentDetail";
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail author="Sam"/>
-            <CommentDetail author="John"/>
-            <CommentDetail author="Mick"/>
+            <CommentDetail
+                author="Sam"
+                timeAgo="Today at 4:45PM"
+                content="Good job, brother!"
+                avatar={faker.image.avatar()}
+            />
+            <CommentDetail
+                author="John"
+                timeAgo="Today at 2:00AM"
+                content="No way! It is fantastic."
+                avatar={faker.image.avatar()}
+            />
+            <CommentDetail
+                author="Mick"
+                timeAgo="Yesterday at 5:00PM"
+                content="It's not bad!"
+                avatar={faker.image.avatar()}
+            />
         </div>
     );
 };
