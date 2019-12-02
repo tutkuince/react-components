73c6ed1cf551e6ecb5c145e016151aef07a1c3b9
tutkuince <tutku.ince@outlook.com>
2019-12-02 21:28:21 +0300

    export default CommentDetail and used in index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import CommentDetail from "./CommentDetail";
 
 const App = () => {
     return (
         <div className="ui container comments">
-
+            <CommentDetail/>
         </div>
     );
 };
